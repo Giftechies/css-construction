e5da30be6cabbc0f06f2f5ef97dfaf83a4043922
Shah097 <manveershah007@gmail.com>
2025-10-08 15:22:41 +0530

    update

--- src/app/(admin)/admin/all-pages/edit/[id]/page.jsx
+++ src/app/(admin)/admin/all-pages/edit/[id]/page.jsx
@@ -1,21 +1,39 @@
 "use client"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import FormCom from "../../component/FormCom"
+import { FetchPageId } from "../../component/action";
+import { Updatepage } from "../../component/action";
+export default function Editpage({params}){
 
-export default function AllPages(){
-
-    // useEffect(()=>{
-
-    // })
-
-    const  handlesubmit = async (data)=>{
-        console.log(data);
-        
+    const [IsinitialData,setIsinitialData] = useState({})
+    useEffect(()=>{
+        async function fetchpage(){
+            const res = await FetchPageId(params.id)
+            console.log(res.data,"ds res>>>>");
+                if (res.success && res.data) {
+      setIsinitialData(res.data); 
     }
+
+        }
+        fetchpage()
+
+    },[])
+  const handleSubmit = async (formData) => {
+    try {
+        console.log("caliin up function");
+        
+      const res = await Updatepage({ data: formData, params });
+      if (res.success) {
+        alert("Page updated successfully!");
+      }
+    } catch (error) {
+      alert("Update failed!");
+    }
+  };
      
     return(
         <div>
-            <FormCom onSubmit={handlesubmit} />
+            <FormCom onSubmit={handleSubmit} initalData={IsinitialData} mode="edit" />
         
         </div>
     )
