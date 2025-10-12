e684f88a10b3f7aa284dd4fb97e99689f67e8189
Shah097 <your-email@example.com>
2025-10-12 17:39:18 +0530

    update

--- src/app/(admin)/admin/all-pages/edit/[id]/page.jsx
+++ src/app/(admin)/admin/all-pages/edit/[id]/page.jsx
@@ -1,40 +1,38 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
-import FormCom from "../../component/FormCom"
+import FormCom from "../../component/FormCom";
 import { FetchPageId } from "../../component/action";
 import { Updatepage } from "../../component/action";
-export default function Editpage({params}){
-
-    const [IsinitialData,setIsinitialData] = useState({})
-    useEffect(()=>{
-        async function fetchpage(){
-            const res = await FetchPageId(params.id)
-            console.log(res.data,"ds res>>>>");
-                if (res.success && res.data) {
-      setIsinitialData(res.data); 
+import { useRouter } from "next/navigation";
+export default function Editpage({ params }) {
+  const [IsinitialData, setIsinitialData] = useState({});
+  const route = useRouter()
+  useEffect(() => {
+    async function fetchpage() {
+      const res = await FetchPageId(params.id);
+      if (res.success && res.data) {
+        setIsinitialData(res.data);
+      }
     }
-
-        }
-        fetchpage()
-
-    },[])
+    fetchpage();
+  }, []);
   const handleSubmit = async (formData) => {
     try {
-        console.log("caliin up function");
-        
+
       const res = await Updatepage({ data: formData, params });
       if (res.success) {
         alert("Page updated successfully!");
+        route.push("/admin/all-pages")
+
       }
     } catch (error) {
       alert("Update failed!");
     }
   };
-     
-    return(
-        <div>
-            <FormCom onSubmit={handleSubmit} initalData={IsinitialData} mode="edit" />
-        
-        </div>
-    )
+
+  return (
+    <div>
+      <FormCom onSubmit={handleSubmit} initalData={IsinitialData} mode="edit" />
+    </div>
+  );
 }
