f16507ab76c56e807b4d72158849a06d8d8f1565
Shah097 <manveershah007@gmail.com>
2025-10-16 09:56:34 +0530

    update

--- src/pages/home/Vision.jsx
+++ src/pages/home/Vision.jsx
@@ -17,7 +17,7 @@ const Vision = () => {
          
           <SectionTitle text="Turning waste into a cleaner, greener future."  className="h1   capitalize " />
           <SectionText className=" text-white-1/60 w-[90%] " text="We cover Central, East, South, West, and North London, including postcodes from EC, WC, W, N, NW, E, SE, SW, CR, BR, UB, HA, TW, IG, and RM."/>
-          <HomeThreeBannerLink href="/pick-details" className="  " >   Get Instant Quote  <IconEdit className=" size-5 theme-transition-3  group-hover:rotate-45" />{" "} </HomeThreeBannerLink>
+          <HomeThreeBannerLink href="/booking-skip-online" className="  " >   Get Instant Quote  <IconEdit className=" size-5 theme-transition-3  group-hover:rotate-45" />{" "} </HomeThreeBannerLink>
           
         </div>
       </div>
