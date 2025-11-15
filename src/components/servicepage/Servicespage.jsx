c08518a7648faa764bc2306dddbda87db1f390de
404manveer <manveershah007@gmail.com>
2025-11-15 17:48:22 +0530

    fix

--- src/components/servicepage/Servicespage.jsx
+++ src/components/servicepage/Servicespage.jsx
@@ -1,24 +1,26 @@
 
 import SectionText from "@/components/shared/SectionText";
 import SectionTitle from "@/components/shared/SectionTitle";
-import React from "react";
 import Link from "next/link";
 import SectionSubTitle from "@/components/shared/SectionSubTitle";
-import { headers } from "next/headers";
+// import { headers } from "next/headers";
+import Image from "next/image";
 const Servicespage = async () => {
-  const cards = [
+
+// const host = headers().get("host")
+//   const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+//   const res1 = await fetch(`${protocol}://${host}/api/service-category`,{
+//     method:'GET'
+//   })
+//   const res= await res1.json()
+const cards = [
     { title: "Skip Hire", content: "We supply a full range of skip sizes for hire in London, from 4-yard mini skips - ideal for small household clearances, through to 8-yard, 10-yard, 12-yard, and 16-yard skips..", link: "/skip-hire",img:"/img/innerimg/skip-hire-pic.jpg" },
     { title: "Concrete Supply", content: "We are trusted concrete suppliers in London, offering ready mix concrete delivery for small domestic jobs to large construction projects.", link: "/concrete-supply",img:"/img/innerimg/concrete.jpg" },
     { title: "Tipper & Grab Services", content: "Our grab hire service is an affordable alternative to skips for heavy waste. Our grab lorries can collect muck, soil, rubble, concrete, hardcore, and green..", link: "/tipper-and-grab-services",img:"/img/innerimg/Tipper-Grab.jpg" },
   
   ];
-const host = headers().get("host")
-  const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
-  const res1 = await fetch(`${protocol}://${host}/api/service-category`,{
-    method:'GET'
-  })
-  const res= await res1.json()
-console.log(res,"d");
+
+  
 
   return (
     <div className=" bg-primary/15 py-10 xl:py-25 ">
@@ -32,36 +34,36 @@ console.log(res,"d");
          
         </div>
         <div className="card flex flex-wrap items-center justify-center gap-10 ">
-          {cards.map((item, id) => {
+          {cards?.map((item, id) => {
             return (
               <div
                 key={id}
                 className="card1 group w-[380px]    overflow-hidden rounded-[2em] border p-4 bg-white-1 sp "
               >
-             <Link href={item.link} >
+             <Link href={`item?.slug`} >
                 <div className="img group h-60 w-full overflow-hidden rounded-3xl bg-red-50 ">
-                  <img
-                    src={item.img}
-                    alt=""
+                  <Image
+                    src={item?.featureImage}
+                    alt={item?.title || "service image" }
                     className=" theme-transition-3  object-cover object-center h-full w-full group-hover:scale-[1.1] "
                   />
                 </div>
              </Link>
                 <div className="text theme-transition-4 flex flex-col items-start justify-center gap-4   p-6  ">
-                 <Link href={item.link} >
-                  <h3 className=" h4 font-[700] text-primary font-oswald ">{item.title}</h3>
+                 <Link href={item?.slug} >
+                  <h3 className=" h4 font-[700] text-primary font-oswald ">{item?.title}</h3>
                  </Link>
                   <SectionText
                     className=" text-[15px] font-[500] text-justify "
                     text={
-                      item.content
+                      item?.discription
                     }
                   />
 
 
                   <div className=" border-t h-[8px]  w-full border-black-3/40 "  >
                   
-                 <Link href={item.link} className=" mt-2 flex text-[14px] gap-2 font-semibold " ><span>Read More</span> <i className="ri-arrow-right-line font-medium text-[18px] "></i>  </Link>
+                 <Link href={item.slug} className=" mt-2 flex text-[14px] gap-2 font-semibold " ><span>Read More</span> <i className="ri-arrow-right-line font-medium text-[18px] "></i>  </Link>
                  </div>
                 </div>
               </div>
