f9d1f604778c2ff8c1fd193825be193d6cec3543
404manveer <manveershah007@gmail.com>
2025-11-20 17:13:47 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,43 @@
-"use client";
-import "@/../node_modules/react-modal-video/scss/modal-video.scss";
-import "@/styles/globals.css";
+// app/layout.tsx
+import "@/styles/globals.css"; // global CSS (server import)
+import "@/../node_modules/react-modal-video/scss/modal-video.scss"; // third-party global styles
 
+import { oswald, manrope } from "@/utils/fonts";
+import Providers from "./Providers"; // client wrapper that contains Redux + Lenis
+import Script from "next/script";
 
-import { Provider } from "react-redux";
-import { store } from "./store/store";
+export const metadata = {
+  title: "CSS Construction",
+  description: "",
+};
 
-import "swiper/css/bundle";
-import "swiper/css/effect-fade";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
-import Footer from "@/components/footer/Footer";
-import { ReactLenis } from "@studio-freight/react-lenis";
-import { oswald,manrope } from "@/utils/fonts";
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`  ${oswald.variable} ${manrope.variable} ${manrope.className}    `}> 
-      <link
-        href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css"
-        rel="stylesheet"
-      />
-      <body>
-      <ReactLenis 
-  root 
-  options={{ smooth: true, smoothTouch: true, duration: 1 }}
->
-    <main>
-      <div className="relative">{children}</div>
-    
-    </main>
+    <html lang="en" className={`${oswald.variable} ${manrope.variable} ${manrope.className}`}>
+      <head>
+        {/* External icon font - OK in head */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css"
+          rel="stylesheet"
+        />
+        {/* Google Tag (recommended to load external script with next/script for performance) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-EXNY5VTVP8"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EXNY5VTVP8');
+          `}
+        </Script>
+      </head>
 
-</ReactLenis>
+      <body>
+        {/* Providers is a client component that supplies Redux, Lenis, etc. */}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
