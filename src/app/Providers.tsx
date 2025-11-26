149b7aa18f0458528da0895c0311f7bf82ea03ab
404manveer <manveershah007@gmail.com>
2025-11-26 14:37:32 +0530

    swipe fix

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,20 +1,23 @@
 
 "use client";
+import "swiper/css/bundle";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import React from "react";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "@/app/store/store"; // adjust path if your store is elsewhere
 import { ReactLenis } from "@studio-freight/react-lenis";
-import Footer from "@/components/footer/Footer";
-
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ReduxProvider store={store}>
       <ReactLenis root options={{ smooth: true, smoothTouch: true, duration: 1 }}>
         <main>
-          <div className="relative min-h-screen flex flex-col">
-            <div className="flex-1">{children}</div>
-            <Footer />
-          </div>
+          {/* <div className="relative min-h-screen flex flex-col"> */}
+            {/* <div className="flex-1"> */}
+              {children}
+              {/* </div> */}
+          {/* </div> */}
         </main>
       </ReactLenis>
     </ReduxProvider>
