4e3698875bab67ab236caf2d40e0fbbdad64cd56
404manveer <manveershah007@gmail.com>
2025-12-08 17:52:09 +0530

    fix

---
+++ src/app/helper/models/serviceCategory.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+const { title } = require("process")
+
+
+const  serviceCategorySchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    slug:{
+        type:String,
+        required:true,
+    },
+    discription:{
+        type:String,
+        required:true
+    },
+    featureImage:{
+        type:String,
+        required:true
+
+    }
+})
+
+const  serviceCategoryModel = mongoose.models.serviceCategory || mongoose.model("serviceCategory",serviceCategorySchema)
+
+export default serviceCategoryModel
